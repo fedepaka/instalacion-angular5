4ffa9682b0a9778b5d18bd6d59103e58fd28c5db
federico ramirez <framirez@folderit.net>
2018-07-06 19:17:25 -0300

    Angular clase 10:Propiedades y tipos de datos en TypeScript y Angular5 - Curso profesional de Angular 5 https://www.youtube.com/watch?v=VJ8g_XcuHg0&index=10&list=PLBdkl5-ytBTxd-00futJ72_vvTESUry8M

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
 })
 
 export class VideojuegosComponent {
-  public nombre_juego: 'Príncipe de Persia';
-  descripcion: 'amigos del barrio';
-  es_retro: false;
-  mejor_juego_retro: 'Super Mario 64.';
-  color: 'yellow';
+  public nombre_juego: string;
+  public descripcion: string;
+  public es_retro: boolean;
+  public mejor_juego_retro: string;
+  public color: string;
+  public year: number;
+  public arrayJuegos: Array<string>;
 
+  constructor() {
+    this.nombre_juego = 'Príncipe de Persia';
+    this.descripcion = 'amigos del barrio';
+    this.es_retro = true;
+    this.mejor_juego_retro = 'Super Mario 64.';
+    this.color = 'red';
+    this.year = 2018;
+    this.arrayJuegos = ['Call of Dutty', 'Los Simpsons', 'Juego 1', 'Juego 2'];
+  }
 }
