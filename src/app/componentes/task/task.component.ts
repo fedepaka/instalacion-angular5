190d2b7f563edce64437f647227e78edb8861afa
framirez <framirez@folderit.net>
2018-07-24 16:28:04 -0300

    - Angular 5 y LocalStorage CRUD | Aplicación desde Cero - https://www.youtube.com/watch?v=rB4uT0P6UF8

--- src/app/componentes/task/task.component.ts
+++ src/app/componentes/task/task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { DataService } from '../../services/data.service';
+import { Task } from '../../models/task';
+
 
 @Component({
   selector: 'app-task',
@@ -6,8 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  @Input('task') task: Task;
 
-  constructor() { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
   }
