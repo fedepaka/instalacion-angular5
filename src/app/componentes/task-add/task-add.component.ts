a00d5a7d2d3564c26cfdb5e9a8c8bae0eba46add
federico ramirez <framirez@folderit.net>
2018-07-25 16:59:49 -0300

    - Angular 5 y LocalStorage CRUD | Aplicación desde Cero - https://www.youtube.com/watch?v=rB4uT0P6UF8 - continuación con algunas mejoras

--- src/app/componentes/task-add/task-add.component.ts
+++ src/app/componentes/task-add/task-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'app-task-add',
@@ -6,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-add.component.css']
 })
 export class TaskAddComponent implements OnInit {
-
+  title: string;
+  description: string;
+  guid: string;
+  @Output() taskAdded = new EventEmitter<Task>();
   constructor() { }
 
   ngOnInit() {
   }
 
   addTask(): void {
+    // se envían los datos al control padre y se limpian los campos
+    this.taskAdded.emit(
+      {
+        title: this.title,
+        description: this.description,
+        guid: Date.now()
+      }
+    );
+    this.title = '';
+    this.description = '';
+    this.guid = '';
   }
 }
